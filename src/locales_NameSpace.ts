8b77334f69127260880e7f42bb59f067080476ef
mahdieh <mahdiehakbari16@gmail.com>
2025-11-09 14:27:00 +0330

    work on structure for language

---
+++ src/locales_NameSpace.ts
@@ -0,0 +1,17 @@
+export const localesNS = [
+  'home',
+  'dental_plane',
+  'login',
+  'profile',
+  'credit',
+  'transaction_list',
+  'request_list',
+  'status',
+  'status_transaction',
+  'my_planes',
+  'payment',
+] as const;
+export type LocaleNS = (typeof localesNS)[number];
+
+export const languages = ['en', 'fa'] as const;
+export type Language = (typeof languages)[number];
