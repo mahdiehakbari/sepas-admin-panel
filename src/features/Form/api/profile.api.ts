02b02a2df5c235a8fa48a0504fc5f9c73f08a86c
d.karimi <d.karimi@sepasholding.com>
2025-12-14 15:16:04 +0330

    fix a bug in DentistDefinition

--- src/features/Form/api/profile.api.ts
+++ src/features/Form/api/profile.api.ts
@@ -7,7 +7,29 @@ export const updateProfile = async (
   token: string,
   data: Partial<IProfileFormValues>,
 ) => {
-  return axios.post(API_UPDATE_PROFILE, data, {
+
+  const requestBody = {
+    merchants: [{
+      phoneNumber: data.mobile,
+      nationalId: data.nationalId,
+      businessName: data.professionalTitle,
+      address: data.addressDetails,
+      cityId: data.cityId,
+      postalCode: data.postalCode,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      iban: data.iban,
+      professionalTitle: data.professionalTitle,
+      workPlacePhoneNumber: data.workPlacePhoneNumber,
+      medicalCertificateNumber: data.medicalCertificateNumber,
+      educationLevel: 0,
+      gender: data.gender,
+      birthDate: data.birthDate
+    }]
+  };
+  
+  return axios.post(API_UPDATE_PROFILE, requestBody, {
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
