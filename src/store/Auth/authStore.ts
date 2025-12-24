3e1fd1ad143701b064799b1b5f4064cc140c0c13
mahdieh <mahdiehakbari16@gmail.com>
2025-12-24 18:34:26 +0330

    work on token exp

--- src/store/Auth/authStore.ts
+++ src/store/Auth/authStore.ts
@@ -1,25 +1,3 @@
-// import { create } from 'zustand';
-// import Cookies from 'js-cookie';
-// import { IAuthState } from './types';
-
-// export const useAuthStore = create<IAuthState>((set) => ({
-//   user: null,
-//   token: Cookies.get('token') || null,
-//   isLoggedIn: !!Cookies.get('token'),
-
-//   setAuth: (token, user) => {
-//     Cookies.set('token', token);
-//     Cookies.set('user', JSON.stringify(user));
-//     set({ token, user, isLoggedIn: true });
-//   },
-
-//   logout: () => {
-//     Cookies.remove('token');
-//     Cookies.remove('phoneNumber');
-//     set({ token: null, user: null, isLoggedIn: false });
-//   },
-// }));
-
 import { create } from 'zustand';
 import Cookies from 'js-cookie';
 import { IAuthState } from './types';
